e9a441ef72b6e56a86e98d2a58808a6055cc431c
huyl <huyl@users.noreply.github.com>
2014-05-13 09:07:07 -0500

    Initial Commit

---
+++ owned-Dizainier/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  owned-Dizainier
+//
+//  Created by Huy on 5/13/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
